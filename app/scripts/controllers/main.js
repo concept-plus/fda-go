e13a396f81e9421404550dae71cfc99e0b2cdc86
Ponnamy Kiep <pkiep@conceptplusllc.com>
2015-06-24 07:11:17 -0400

    BPA-37: Refactored left nav

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,9 +7,29 @@
  * # MainCtrl
  * Controller of the fdagoApp
  */
-angular.module('fdagoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.search = [];
+angular.module('fdagoApp').controller('MainCtrl', function($scope, $location) {
+    // hide nav for home screen.
+    //angular.element('#navigation').hide();
 
-    angular.element('#navigation').hide();
-  });
+    this.queryCategory = 'drug';
+    this.searchString = null;
+
+    var navigateToResults = function(category, search) {
+        var path = '/results/' + category;
+        if (('' + search).length > 0) {
+            path = path + '/' + search;
+        }
+        console.log('navigating to ' + path);
+        $location.path(path);
+    };
+
+    this.submitSearch = function() {
+        navigateToResults(this.queryCategory, this.searchString);
+    };
+
+    this.submitDrugRecallQuery = angular.bind(null, navigateToResults, 'drug');
+
+    this.submitDeviceRecallQuery = angular.bind(null, navigateToResults, 'device');
+
+    this.submitFoodRecallQuery = angular.bind(null, navigateToResults, 'food');
+});
