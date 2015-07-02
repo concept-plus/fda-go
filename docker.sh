cadb459727339b5939e5f189b1b68bbcce10b3dd
Alex Rangeo <arangeo@conceptplusllc.com>
2015-07-02 16:26:28 -0400

    Updated docker.sh to stop, remove and add a name to the new container.

--- docker.sh
+++ docker.sh
@@ -1,4 +1,8 @@
 #Script for running docker and uploading latest container to docker hub
+##########################
+#This script assumes that the container has been started and is running
+#And has the name fdago-dev. If not, the script will fail.
+##########################
 echo "---------------------"
 echo "Building Docker Image"
 echo "---------------------"
@@ -9,7 +13,13 @@ echo "Pushing Docker Image to Docker Hub"
 echo "----------------------------------"
 docker push conceptplus/fdago-dev
 
+echo "-----------------------------------------"
+echo "Stopping the Current Running Docker Image"
+echo "-----------------------------------------"
+docker stop fdago-dev
+docker rm fdago-dev
+
 echo "--------------------------------------------"
 echo "Running Docker Image - conceptplus/fdago-dev"
 echo "--------------------------------------------"
-docker run -p 80:80 conceptplus/fdago-dev
+docker run -p 80:80 --name fdago-dev conceptplus/fdago-dev
